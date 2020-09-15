afa1821b71a7c1dd5f469451f456bda0633bc3e0
Clauber <clauber.cesario@gmail.com>
2020-09-15 17:38:27 -0300

    first commit

---
+++ src/main/java/br/sp/gov/etec/controller/AlunoController.java
@@ -0,0 +1,38 @@
+package br.sp.gov.etec.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.sp.gov.etec.dto.AlunoDto;
+import br.sp.gov.etec.entity.Aluno;
+import br.sp.gov.etec.service.AlunoService;
+
+@RestController
+@RequestMapping("/api/aluno")
+public class AlunoController {
+
+	@Autowired
+	AlunoService service;
+	
+	@PostMapping
+	public ResponseEntity<?> cadastrarCurso(@RequestBody AlunoDto dto){		
+		Aluno aluno = service.cadastrarAluno(dto);		
+		return ResponseEntity.status(HttpStatus.OK).body(aluno);
+	}	
+	
+	
+	@GetMapping
+	public ResponseEntity<?> listaCurso(){		
+		List<Aluno> aluno = service.listAlunos();	
+		return ResponseEntity.status(HttpStatus.OK).body(aluno);
+	}	
+	
+}
